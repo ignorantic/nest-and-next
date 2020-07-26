9c298082893ac622d46a8a4673dd08f7c91798f7
Andrii Sorokin <ansolaris@gmail.com>
2020-07-26 11:20:51 +0300

    Add: fromRange helper

--- src/database/factories/posts.factory.ts
+++ src/database/factories/posts.factory.ts
@@ -2,10 +2,11 @@ import * as Faker from 'faker';
 import { define } from 'typeorm-seeding';
 import { capitalize } from '@material-ui/core';
 import { Post } from '../../backend/posts/posts.entity';
+import { fromRange } from '../../common/helpers';
 
 define(Post, (faker: typeof Faker) => {
   const post = new Post();
-  post.title = capitalize(faker.lorem.words(Math.round(Math.random() * 10) + 2));
-  post.text = faker.lorem.sentences(Math.round(Math.random() * 50) + 5);
+  post.title = capitalize(faker.lorem.words(fromRange(2, 10)));
+  post.text = faker.lorem.sentences(fromRange(5, 20));
   return post;
 });
